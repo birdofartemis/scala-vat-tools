fd1608b646259d6fd2781dd0ebb2e556b25ce855
Marco Filipe Alonso Medeiros <72250724+birdofartemis@users.noreply.github.com>
2026-04-15 10:36:27 +0000

    feat: create vat rule validations for Portugal

---
+++ src/main/scala/org/vat/tools/domain/model/validations/CheckDigitRuleValidation.scala
@@ -0,0 +1,29 @@
+package org.vat.tools.domain.model.validations
+
+import org.vat.tools.domain.model.value.objects.Rule
+
+private[model] trait CheckDigitRuleValidation {
+  def checkDigitRule(vatNumber: String): Rule = {
+    val digits = vatNumber.map(_.asDigit)
+
+    val weightSum =
+      digits
+        .take(8) // fist 8 digits
+        .zip(9 to 2 by -1)
+        .map { case (digit, weight) => digit * weight }
+        .sum
+
+    val remainder = weightSum % 11
+
+    val expectedDigit =
+      if (remainder < 2) 0 else 11 - remainder
+
+    Rule(
+      label = "check-digit-rule",
+      condition = _ => digits.last == expectedDigit,
+      vatNumber = vatNumber,
+      errorMessage =
+        s"The provided vat number: [$vatNumber] with last digit: [${digits.last}] is not valid"
+    )
+  }
+}
